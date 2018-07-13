e85b95f42a90a9c2536e9c6062d6281e23e1694c
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-13 18:47:52 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=543;
+		int c=0;
+		while(n!=0)
+		{
+			n=n/10;
+			c++;
+		}
+		System.out.println(c);
+	}
+}
