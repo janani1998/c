286cc9e7d6e21455d0a1360955d0e61eee2a2e81
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-13 18:10:49 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=2013;
+		if(n%4==0)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+				System.out.print("No");
+		}
+	}
+}
