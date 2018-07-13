fe5c8545d600b64e19e031cf8fd26a6bc7681f55
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-13 18:32:45 +0530

    Create Naturalno.java

---
+++ Naturalno.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       int n=10;
+       int sum=0;
+       for(int i=0;i<=n;i++)
+       {
+       	sum=sum+i;
+       }
+       	System.out.print(sum);
+       
+	}
+}
