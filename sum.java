67c9e681147b40a08cd47319b47d48683e3f6314
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-13 18:41:12 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=5;
+		int sum=0;
+		for(int i=0;i<=5;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
